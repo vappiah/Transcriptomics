0fc541b99a4db6041cb8e51c8898528fa2f9b640
vincentappiah <appiahv@rocketmail.com>
2022-04-01 20:33:46 +0200

    Add files via upload

---
+++ Tutorials/gene-expression-analysis/Episode-1/get_scripts.sh
@@ -0,0 +1,7 @@
+
+github_page=https://raw.githubusercontent.com/vappiah/Transcriptomics/main/Tutorials/gene-expression-analysis/Episode-1
+for script in assemble_transcripts.sh estimate_abundance.sh map_reads.sh prepare_data.sh sort_mapped_reads.sh visualize_deg.R 
+do
+
+wget "$github_page"/$script
+done
