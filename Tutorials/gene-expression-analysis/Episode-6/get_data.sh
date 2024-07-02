a36328b5d4747c91170964cc831fa668251571ef
vincentappiah <appiahv@rocketmail.com>
2024-07-02 18:22:11 +0000

    Update get_data.sh

--- Tutorials/gene-expression-analysis/Episode-6/get_data.sh
+++ Tutorials/gene-expression-analysis/Episode-6/get_data.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
+#WARNING: This script is for education purposes. You modify this script at your own risk
 
+#we are interested in the raw-data(samples) and reference genome(fasta and gff) and phenotype information. 
+#So we will put them in a directory called resources.All other files will be removed
+
+wget  ftp://ftp.ccb.jhu.edu/pub/RNAseq_protocol/chrX_data.tar.gz
+tar xvfz chrX_data.tar.gz
+
+resourcedir=resources
+mkdir $resourcedir
+
+
+#mv chrX_data/geuvadis_phenodata.csv $resourcedir
+mv -v chrX_data/samples ./fastq 
+mkdir ref
+mv chrX_data/genes/chrX.gtf ref
+mv chrX_data/genome/chrX.fa ref
+
+
+mv -v chrX_data/samples $resourcedir
+mkdir $resourcedir/ref
+mv chrX_data/genes/chrX.gtf $resourcedir/ref
+mv chrX_data/genome/chrX.fa $resourcedir/ref
+
+
+#remove unwanted files
+rm -fr chrX_data
+
+echo "data preparating is completed"
+echo "all data can be found in the directory : \"resources\" " 
